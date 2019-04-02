ab9ab5de3daac59eeca58803e8a78a7159b03d59
901att <49183767+901att@users.noreply.github.com>
2019-04-02 19:05:50 +1100

    Renew comments

--- js/script.js
+++ js/script.js
@@ -1,58 +1,6 @@
-/******************************************
-Treehouse FSJS Techdegree:
-project 1 - A Random Quote Generator
-******************************************/
-
-// Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
-
-
-/***
-  Create the array of quote objects and name it `quotes`.
-  Add at least five quote objects to the `quotes` array.
-  Give each quote object a `quote` and `source` property.
-  Add the `citation` property to at least one object in the array.
-  Add the `year` property to at least one object in the array.
-  Use console.log() to log your array of quotes to the console.
-***/
-
-
-
-
-/***
-  Create the `getRandomQuote` function to:
-   - Create a variable to store a random number
-   - Cse the random number to `return` a random quote object from the `quotes` array.
-***/
-
-
-
-
-/***
-  Create the `printQuote` function to:
-   - Call the `getRandomQuote` function and assign it to a variable.
-   - Create a variable for the HTML string and set it equal to an empty string.
-   - Use the HTML template in the instructions or the markup in the index.html file, AND
-     the random quote vairable to build your HTML string.
-   - Add the quote and source section to the HTML string.
-   - Use an if statement to check for the citation property before adding it to the HTML string.
-   - Use an if statement to check for the year property before adding it to the HTML string.
-   - Don't forget to close that final `p` tag.
-   - Set the `innerHTML` of the `quote-box` div to the HTML string.
-***/
-
-
-
-
-/***
-  When the "Show another quote" button is clicked, the event listener
-  below will be triggered, and it will call, or "invoke", the `printQuote`
-  function. So do not make any changes to the line of code below this
-  comment.
-***/
-
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+//quotes from famouse movies
 var quotes = [
   {
     quote:"I'm the king of the world!" ,
@@ -98,11 +46,13 @@ var quotes = [
   }
 ];
 
+//A function for generate a random number for quote to access object
 function getRandomQuote(arr){
   var randomNumber = Math.floor(Math.random() * quotes.length);
   return arr[randomNumber];
 }
 
+//A function for generate a random rgb color
 function random_Rgb(){
   var r = Math.floor(Math.random()*256);
   var g = Math.floor(Math.random()*256);
@@ -110,6 +60,10 @@ function random_Rgb(){
   var rgbColor = "rgb(" + r + "," + g + "," + b + ")";
   return rgbColor;
 }
+
+/*When click "show another quote" buttom will trigger this function to create a random quote and it's source
+  or wait for 5 seconds it will renew itself with random background and buttom color. 
+*/
 
 function printQuote(){
   var randomQuote = getRandomQuote(quotes);
